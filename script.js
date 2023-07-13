f7f6fa29912d0ad437a9dc182d43d2cec2109920
milodevhh <schmiesseklgmail.com>
2023-07-13 12:24:41 +0200

    fixed date

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
-document.querySelector('[data-js="year"]').textContent =
+const dateElement = document.querySelector('[data-js="date"]');
 const date = new Date().toLocaleDateString("en-US");
 dateElement.textContent = date;
